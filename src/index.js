cafec9ca935874ff2fc74702d1a7676f192cca50
pato <patricio.dfernandez@gmail.com>
2021-09-27 16:07:23 -0300

    instalando todo

--- src/index.js
+++ src/index.js
@@ -2,22 +2,24 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import path from "path";
-import "./database";
-import productoRouter from "./routes/producto.routes";
+import './database';
+import productoRouter from './routes/producto.routes'
 
 const app = express();
 
-app.use(morgan("dev"));
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(morgan('dev'))
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, '../public')))
 
-app.set("port", process.env.PORT || 4000);
 
-app.use("/api/cafeteria", productoRouter);
+app.set('port', process.env.PORT || 4000)
 
-app.listen(app.get("port"), () => {
-  // console.log(path.join(__dirname, '../public'))
-  console.log("estoy escuchando el puerto ", +app.get("port"));
-});
+
+app.use('/api/cafeteria',productoRouter)
+
+app.listen(app.get('port'), () => {
+    // console.log(path.join(__dirname, '../public'))
+    console.log('estoy escuchando el puerto ', +app.get('port'))
+})
